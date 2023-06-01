96f720a811396a3c82484449a560731e17264c1c
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-06-01 20:20:03 +0900

    project_create

---
+++ BackEnd/Coded/src/test/java/kh/coded/CodedApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.coded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
