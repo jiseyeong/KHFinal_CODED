a9416a9ac5441cf8f2c1232c196b39b49646076e
jiseyeong <hanyu3677@naver.com>
2023-06-05 16:47:03 +0900

    주석 처리
    주석 처리

--- BackEnd/Coded/src/main/java/kh/coded/services/MemberService.java
+++ BackEnd/Coded/src/main/java/kh/coded/services/MemberService.java
@@ -1,45 +1,47 @@
-package kh.coded.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import kh.coded.dto.MemberDTO;
-import kh.coded.repositories.MemberDAO;
-
-public class MemberService implements UserDetailsService {
-
-	@Autowired
-	private MemberDAO memberDAO;
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		MemberDTO user = memberDAO.selectMemberById(username);
-		if(user == null) {
-			throw new UsernameNotFoundException(username + "은 없는 회원입니다.");
-		}
-		return User.builder()
-				.username(user.getUserID())
-				.password(user.getPw())
-				.roles(user.getRole().getValue())
-				.build();
-	}
-	
-	public MemberDTO selectByID(String userID) {
-		return memberDAO.selectMemberById(userID);
-	}
-	
-	public MemberDTO selectByUserNo(int userNo) {
-		return memberDAO.selectMemberByUserNo(userNo);
-	}
-	
-	public int join(MemberDTO dto) {
-		dto.setPw(passwordEncoder.encode(dto.getPw()));
-		return memberDAO.insertMemeber(dto);
-	}
-}
+//package kh.coded.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import kh.coded.dto.MemberDTO;
+//import kh.coded.repositories.MemberDAO;
+//
+//@Service
+//public class MemberService implements UserDetailsService {
+//
+//	@Autowired
+//	private MemberDAO memberDAO;
+////	@Autowired
+////	private PasswordEncoder passwordEncoder;
+//	
+////	@Override
+////	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+////		MemberDTO user = memberDAO.selectMemberById(username);
+////		if(user == null) {
+////			throw new UsernameNotFoundException(username + "은 없는 회원입니다.");
+////		}
+////		return User.builder()
+////				.username(user.getUserID())
+////				.password(user.getPw())
+////				.roles(user.getRole().getValue())
+////				.build();
+////	}
+//	
+//	public MemberDTO selectByID(String userID) {
+//		return memberDAO.selectMemberById(userID);
+//	}
+//	
+//	public MemberDTO selectByUserNo(int userNo) {
+//		return memberDAO.selectMemberByUserNo(userNo);
+//	}
+//	
+////	public int join(MemberDTO dto) {
+////		dto.setPw(passwordEncoder.encode(dto.getPw()));
+////		return memberDAO.insertMemeber(dto);
+////	}
+//}
