18e46729a85db882c3159c69cd48d6531c735c57
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-06-05 13:47:43 +0900

    06.05 login middle save

--- BackEnd/Coded/src/main/java/kh/coded/config/SecurityConfiguration.java
+++ BackEnd/Coded/src/main/java/kh/coded/config/SecurityConfiguration.java
@@ -1,31 +1,31 @@
-//package kh.coded.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import jakarta.servlet.DispatcherType;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfiguration {
-//	
+package kh.coded.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.DispatcherType;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
 //	@Autowired
 //	private JwtProvider jwtProvider;
 //	@Autowired
 //	private UserDetailsService userService;
-//	
+	
 //	@Bean
 //	public WebSecurityCustomizer configure() {
 //		return (web) -> web.ignoring()
@@ -37,7 +37,7 @@
 //					.requestMatchers("/favicon*/**")
 //					.requestMatchers("/img/**");
 //	}
-//	
+	
 //	@Bean
 //	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 //		http.csrf().disable().cors().disable()
@@ -64,7 +64,7 @@
 //		
 //		return http.build();
 //	}
-//	
+	
 //	@Bean
 //	public AuthenticationManager authenticationManager(
 //			HttpSecurity http,
@@ -77,10 +77,10 @@
 //					.and()
 //					.build();
 //	}
-//	
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//	
-//}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
--- BackEnd/Coded/src/main/java/kh/coded/controllers/AuthenticationController.java
+++ BackEnd/Coded/src/main/java/kh/coded/controllers/AuthenticationController.java
@@ -1,44 +1,39 @@
-//package kh.coded.controllers;
-//
-//import java.net.URLEncoder;
-//import java.util.Map;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import kh.coded.config.JwtProvider;
-//import kh.coded.dto.MemberDTO;
-//import kh.coded.dto.RefreshTokenDTO;
-//import kh.coded.repositories.TokenDTO;
-//import kh.coded.services.MemberService;
-//import kh.coded.services.RefreshTokenService;
-//import utils.CookieUtil;
-//
-//@RestController
-//@RequestMapping("/auth/")
-//public class AuthenticationController {
-//	
+package kh.coded.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kh.coded.services.MemberService;
+import kh.coded.services.RefreshTokenService;
+
+@RestController
+@RequestMapping("/auth/")
+public class AuthenticationController {
+	
 //	@Autowired
 //	private JwtProvider jwtProvider;
-//	@Autowired
-//	private RefreshTokenService refService;
-//	@Autowired
-//	private MemberService memberService;
-//	
-//	// JWT 토큰 발급
-//	@PostMapping(value="login")
-//	public void login(
-//			@RequestBody Map<String, Object> input,
-//			HttpServletRequest request,
-//			HttpServletResponse response) throws Exception{
-//		
-//		//로그인 로직 실행
-//		
+	@Autowired
+	private RefreshTokenService refService;
+	@Autowired
+	private MemberService memberService;
+	
+	// 로그인 및 JWT 토큰 발급
+	@PostMapping(value="login")
+	public void login(
+			@RequestBody Map<String, Object> input,
+			HttpServletRequest request,
+			HttpServletResponse response) throws Exception{
+		
+		//로그인 로직 실행
+		
+		
 //		MemberDTO dto = new MemberDTO();
 //		//jwt로직
 //		TokenDTO tokens = jwtProvider.createAllLoginToken(dto);
@@ -49,8 +44,8 @@
 //		
 //		RefreshTokenDTO refresh = new RefreshTokenDTO(0, dto.getUserNo(), "Bearer " + refreshToken);
 //		refService.save(refresh);
-//	}
-//	
+	}
+	
 //	@PostMapping(value="refresh")
 //	public void refresh(
 //			@RequestBody Map<String, Object> input,
@@ -91,6 +86,5 @@
 //			
 //			CookieUtil.addSecureCookie(response, "Coded-Token", "Bearer " + accessToken, 60 * 60 * 24 * 1);
 //		}
-//		
 //	}
-//}
+}
