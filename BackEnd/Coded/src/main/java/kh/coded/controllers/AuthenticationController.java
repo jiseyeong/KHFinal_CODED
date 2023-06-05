1f439714aa89660ac08c26a6273d0b9ed846e01f
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-06-05 14:30:43 +0900

    Update AuthenticationController.java

--- BackEnd/Coded/src/main/java/kh/coded/controllers/AuthenticationController.java
+++ BackEnd/Coded/src/main/java/kh/coded/controllers/AuthenticationController.java
@@ -1,15 +1,15 @@
 package kh.coded.controllers;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kh.coded.dto.MemberDTO;
+import kh.coded.dto.MemberDTO.Role;
 import kh.coded.services.MemberService;
 import kh.coded.services.RefreshTokenService;
 
@@ -24,12 +24,25 @@ public class AuthenticationController {
 	@Autowired
 	private MemberService memberService;
 	
+	@PostMapping(value="join")
+	public void join(
+			@RequestParam(value="userID") String id,
+			@RequestParam(value="pw") String pw,
+			@RequestParam(value="address1") String address1,
+			@RequestParam(value="address2") String address2
+			) {
+		//userNo, id, pw, nickname, bio, favBrand, Address1, Address2, NaverToken, KakaoToken, Role
+		MemberDTO dto = new MemberDTO(0, id, pw, null, null, null, address1, address2, null, null, Role.USER);
+	}
+	
 	// 로그인 및 JWT 토큰 발급
 	@PostMapping(value="login")
 	public void login(
-			@RequestBody Map<String, Object> input,
 			HttpServletRequest request,
-			HttpServletResponse response) throws Exception{
+			HttpServletResponse response,
+			@RequestParam(value="userID") String id,
+			@RequestParam(value="pw") String pw
+			) throws Exception{
 		
 		//로그인 로직 실행
 		
