1ef3a9da930dfccd2753cc68f630619fac9005cc
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-06-06 17:32:56 +0900

    join setting

---
+++ FrontEnd/coded/src/component/auth/Join.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const JoinPageComponent = ()=>{
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+      )
+}
+
+export default JoinPageComponent;
