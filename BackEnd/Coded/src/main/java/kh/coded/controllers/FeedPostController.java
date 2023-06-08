f12d713a780c9bd7567bc666b24d41f756c07cb6
Heeeesam <dltkdn97@gmail.com>
2023-06-08 16:51:08 +0900

    modify feedlist sample

--- BackEnd/Coded/src/main/java/kh/coded/controllers/FeedPostController.java
+++ BackEnd/Coded/src/main/java/kh/coded/controllers/FeedPostController.java
@@ -1,6 +1,7 @@
 package kh.coded.controllers;
 
 import kh.coded.dto.FeedPostDTO;
+import kh.coded.dto.MemberDTO;
 import kh.coded.services.FeedPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ public class FeedPostController {
     @Autowired
     private FeedPostService feedPostService;
 
-    @GetMapping("/selectfeedlist/{id}/")
+    @GetMapping("/selectfeedlist/")
     public List<FeedPostDTO> selectFeedList(){
         List<FeedPostDTO> list = feedPostService.selectTestFeedList();
         return list;
