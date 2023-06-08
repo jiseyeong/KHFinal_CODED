c24af047c7a47160522a669ff5928e56739442cc
KimDaYoung312 <xkdy312x@naver.com>
2023-06-08 16:40:46 +0900

    Update

--- BackEnd/Coded/src/main/java/kh/coded/controllers/FeedPostController.java
+++ BackEnd/Coded/src/main/java/kh/coded/controllers/FeedPostController.java
@@ -4,13 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kh.coded.dto.FeedPostDTO;
+import kh.coded.dto.HashTagDTO;
+import kh.coded.dto.TodayWeatherDTO;
+
 @RestController
 @RequestMapping("/feedpost/")
 public class FeedPostController {
 
-    @GetMapping("/selectfeedlist")
-    public String selectFeedList(){
-        return "";
-    }
-}
+	@GetMapping("/selectfeedlist")
+	public String selectFeedList() {
+		return "";
+	}
 
+	@GetMapping("/insertfeedpost")
+	public String insertFeedPost(FeedPostDTO fdto, HashTagDTO hdto) {
+		return "";
+	}
+	
+}
