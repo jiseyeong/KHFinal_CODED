5ab38ef56ffe8a8e268403b157f3e62c5ea98660
Heeeesam <dltkdn97@gmail.com>
2023-06-13 18:57:49 +0900

    merge Header and NaviBar
     merge Header and NaviBar

--- FrontEnd/coded/src/pages/test/FeedListByHashsWithMain.js
+++ FrontEnd/coded/src/pages/test/FeedListByHashsWithMain.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Footer from '../../pages/containers/Footer';
-import Header from '../../pages/containers/Header';
 import GlobalStyles from '../../styles/GlobalStyles';
 import { styled } from 'styled-components';
-import Navigator from '../../pages/containers/Navbar';
 import FeedListByHashs from '../feedList/FeedListByHashs';
+import Header from '../../component/Main/Header';
+import Footer from '../../component/Main/Footer';
 
 const Container = styled('div')`
   width: 100%;
