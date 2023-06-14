cb72317c71d4c72a313d15379feb508001005de3
Sungha Kim <sungha0067@gmail.com>
2023-06-14 21:32:41 +0900

    updated

--- FrontEnd/coded/src/Routes.js
+++ FrontEnd/coded/src/Routes.js
@@ -16,29 +16,37 @@ import FeedListByIdWithMain from './test/FeedListByIdWithMain';
 import FeedListByHashsWithMain from './test/FeedListByHashsWithMain';
 import FeedListByNickNameWithMain from './test/FeedListByNickNameWithMain';
 import FileUploadTest from './test/FileUploadTest';
+import KakaoCodeCallbackPage from "./pages/auth/OAuthKakaoCodeCallback";
+import LastCallbackPage from "./pages/auth/OAuthLastCallback";
+import NaverCodeCallbackPage from "./pages/auth/OAuthNaverCodeCallback";
 
 class Routes extends React.Component {
   render() {
     return (
         <Router>
          <Switch>
-           <Route exact path="/" component={IndexPage} />
-           <Route exact path="/login" component={Login} />
-           <Route exact path="/signup" component={SignUp} />
-           <Route exact path="/feed" componentment={FeedList} />
-           <Route exact path="/post" componentent={PostDetail} />
-           <Route exact path="/search" component={Search} />           
-           <Route exact path="/profile" component={Profile} />
-           <Route exact path="/ootd" component={Ootd} />
+           <Route exact path="/" element={<IndexPage />} />
+           <Route exact path="/login" element={<Login />} />
+           <Route exact path="/signup" element={<SignUp />} />
+           <Route exact path="/feed" element={<FeedList />} />
+           <Route exact path="/post" element={<PostDetail />} />
+           <Route exact path="/search" element={<Search />} />           
+           <Route exact path="/profile" element={<Profile/>} />
+           <Route exact path="/ootd" element={<Ootd/>} />
            <Route exact path="/FeedList" element={<FeedList />} />
            <Route exact path="/FeedList/id" element={<FeedListByIdWithMain />} />
            <Route exact path="/FeedList/nick" element={<FeedListByNickNameWithMain />} />
            <Route exact path="/FeedList/hashs" element={<FeedListByHashsWithMain />} />
+
+           <Route path="/FileUploadTest" element={<FileUploadTest />} />
+           <Route path="/login/oauth2/code/kakao" element={<KakaoCodeCallbackPage/>} />
+           <Route path="/login/oauth2/callback/kakao" element={<LastCallbackPage/>} />
+           <Route path="/login/oauth2/code/naver" element={<NaverCodeCallbackPage/>} />
+           <Route path="/login/oauth2/callback/naver" element={<LastCallbackPage/>} />
+           <Route path="/DMPage" element={<DMPage />} />
          </Switch>
        </Router>
   );
 }
-
-
 }
 export default Routes;
