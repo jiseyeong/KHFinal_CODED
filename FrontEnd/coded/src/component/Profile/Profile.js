9d08f50890c7dc0146d5081f48586ce47c4564cc
Heeeesam <dltkdn97@gmail.com>
2023-06-22 16:47:19 +0900

    NaviBar의 날씨 표시하는 div 임시로 주석처리함

--- FrontEnd/coded/src/component/Profile/Profile.js
+++ FrontEnd/coded/src/component/Profile/Profile.js
@@ -6,20 +6,22 @@ const ProfileTemplateBlock = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: #f2f2f2;
+  margin: 0;
+  padding: 2rem;
 `;
 
 const WhiteBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 2rem;
-  margin: 0;
+  flex-direction: column;
   width: 100%;
 `;
 
 const ProfileFormBlock = styled.div`
-  width: 50%;
-  height: 400px;
+  width: 60%;
+  margin: 1rem;
   border-radius: 10px;
   background-color: #ffffff;
   box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.1);
@@ -30,37 +32,20 @@ const ProfileTemplate = () => {
     <ProfileTemplateBlock>
       <WhiteBox>
         <ProfileFormBlock>
-          <div className={styles.container}>
-            <div className={styles.imageBox}>
-              <div className={styles.proImage}>
-                <img src=""></img>
-                // 프로필 사진 이미지
+          <div className={styles.profileContainer}>
+            <div className={styles.title}>프로필</div>
+            <div className={styles.info}>
+              <div className={styles.profile}>
+                <div className={styles.profile1}>
+                  <img></img>
+                </div>
+                <div className={styles.profile2}>
+                  <button>사진 바꾸기</button>
+                </div>
               </div>
-              <div className={styles.btns}>
-                <button className={styles.profileImageChangebtn}>
-                  변경하기
-                </button>
-              </div>
-            </div>
-
-            <div className={styles.infoBox}>
-              <div className={styles.infocontents}>
-                <ul>
-                  <li>nick : </li>
-                  <li>id : </li>
-                  <li>pw : </li>
-                  <li>bio : </li>
-                  <li>hashtag : </li>
-                  <li>location : </li>
-                </ul>
-              </div>
-
-              <div className={styles.btns}>
-                <button className={styles.PwChangeBtn}>비밀번호 변경</button>
-                <button className={styles.EditBtn}>수정하기</button>
-                <button className={styles.EditCancelBtn}>수정취소</button>
-                <button className={styles.EditComBtn}>수정완료</button>
-                <button className={styles.MemberOutBtn}>회원탈퇴</button>
+              <div className={styles.infoLayout}>
+                <div className={styles.email}>dltkdn97@naver.com</div>
+                <div className={styles.name}>이상우</div>
               </div>
             </div>
           </div>
