7fa47be78c2016948c238415814ed0f5b3f4d07c
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-06-23 15:46:29 +0900

    today weather move from navi to component

--- FrontEnd/coded/src/App.js
+++ FrontEnd/coded/src/App.js
@@ -31,6 +31,7 @@ import FeedCommentList from './component/FeedPostDetail/FeedCommentList';
 
 import CaroselTest from './test/CaroselTest';
 import FeedInsertTest from './test/FeedInsertTest';
+import TodayAndAdForm from './component/TodayAndAd/TodayAndAdForm';
 
 function App() {
   const dispatch = useDispatch();
@@ -100,6 +101,7 @@ function App() {
           element={<FeedCommentList feedPostId={295} depth={0} parentId={0} />}
         />
         <Route path="/test/FeedInsert" element={<FeedInsert />} />
+        <Route path="/test/TodayAndAdForm" element={<TodayAndAdForm/>}/>
 
         <Route
           path="/login/oauth2/code/kakao"
