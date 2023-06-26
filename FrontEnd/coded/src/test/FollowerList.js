f0c8b730db3c21104a485ca5f6493c566dd66bb7
KimDaYoung312 <xkdy312x@naver.com>
2023-06-26 10:57:37 +0900

    commit

---
+++ FrontEnd/coded/src/test/FollowerList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function FollowerList() {
+  return (
+    <div className="container">
+      <div
+        style={{
+          border: '1px solid black',
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div style={{ border: '1px solid black', width: '20%' }}>사진</div>
+        <div style={{ border: '1px solid black', width: '50%' }}>
+          <div>아이디</div>
+          <div>닉네임</div>
+        </div>
+        <div style={{ border: '1px solid black', width: '30%' }}>
+          <button>팔로워 버튼</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FollowerList;
