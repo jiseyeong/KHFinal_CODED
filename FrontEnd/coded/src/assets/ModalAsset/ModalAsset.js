9d34124ca4cfff5d55af0cefe5d46d1cadd208f5
Heeeesam <dltkdn97@gmail.com>
2023-06-27 01:04:29 +0900

    update feedlist feedlike

--- FrontEnd/coded/src/assets/ModalAsset/ModalAsset.js
+++ FrontEnd/coded/src/assets/ModalAsset/ModalAsset.js
@@ -89,24 +89,24 @@ const Temperature = () => {
       <defs />
       <g
         fill="none"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         id="Thermometer-Hot"
         stroke="none"
-        stroke-width="1"
+        strokeWidth="1"
       >
         <g
           id="Group"
           stroke="#979797"
-          stroke-width="2"
+          strokeWidth="2"
           transform="translate(7.000000, 2.000000)"
         >
           <path
             d="M3,16.8026932 L3,3 C3,1.34314575 4.34314575,3.04359188e-16 6,0 C7.65685425,-3.04359188e-16 9,1.34314575 9,3 L9,16.8026932 C10.7934041,17.8401214 12,19.7791529 12,22 C12,25.3137085 9.3137085,28 6,28 C2.6862915,28 0,25.3137085 0,22 C0,19.7791529 1.20659589,17.8401214 3,16.8026932 Z"
             id="Combined-Shape"
           />
-          <path d="M13,5 L18,5" id="Path-19" stroke-linecap="round" />
-          <path d="M13,9 L18,9" id="Path-20" stroke-linecap="round" />
-          <path d="M13,13 L18,13" id="Path-21" stroke-linecap="round" />
+          <path d="M13,5 L18,5" id="Path-19" strokeLinecap="round" />
+          <path d="M13,9 L18,9" id="Path-20" strokeLinecap="round" />
+          <path d="M13,13 L18,13" id="Path-21" strokeLinecap="round" />
         </g>
         <g
           fill="#CCCCCC"
@@ -123,6 +123,7 @@ const Temperature = () => {
 
 const ModalAsset = () => {
   return <div></div>;
+  l;
 };
 
 export default ModalAsset;
