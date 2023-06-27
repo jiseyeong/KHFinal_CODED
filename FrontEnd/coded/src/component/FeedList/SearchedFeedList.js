8dfa5cdf9b599f9f424fd60948400096f69e3419
Heeeesam <dltkdn97@gmail.com>
2023-06-27 17:25:39 +0900

    middle save

--- FrontEnd/coded/src/component/FeedList/SearchedFeedList.js
+++ FrontEnd/coded/src/component/FeedList/SearchedFeedList.js
@@ -45,18 +45,12 @@ const FeedPostOuter = styled('div')`
 
 function SearchedFeedList() {
   const [feedPost, setFeedPost] = useState([]);
-  // const [thumbNail, setThumbnail] = useState([]);
-  // const [member, setMember] = useState([]);
-  // const [userProfile, setUserProfile] = useState([]);
-  // const [hashTagList, setHashTagList] = useState([]);
-  // const [columnHeights, setColumnHeights] = useState([0, 0, 0, 0, 0]);
   const [newSearch, setNewSearch] = useState(false);
   const feedPostOuterRef = useRef(null);
-  // const [cpage, setCpage] = useState(1);
   const cpage = useRef(1);
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const keyword = searchParams.get('keyword');
+  const keyword = searchParams.get('userId');
   let pageLoading = false;
 
   // useEffect(() => {
@@ -65,7 +59,7 @@ function SearchedFeedList() {
   useEffect(() => {
     cpage.current = 1;
     setNewSearch(true);
-    addSearchedFeedList(keyword);
+    addSearchedFeedList(userId);
     console.log(keyword);
     window.onscroll = function () {
       if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
@@ -80,7 +74,7 @@ function SearchedFeedList() {
 
   // 현재 위치 (현재 페이지) 별 피드 리스트 출력
   const addSearchedFeedList = (keyword) => {
-    if(!pageLoading){
+    if (!pageLoading) {
       pageLoading = true;
       axios
         .request({
@@ -91,46 +85,23 @@ function SearchedFeedList() {
           },
         })
         .then((resp) => {
-          // console.log(resp.data);
-          // const {
-          //   feedPostList,
-          //   thumbNailList,
-          //   memberList,
-          //   userProfileList,
-          //   hashTagLists,
-          // } = resp.data;
           pageLoading = false;
           // 새로 검색했을 때,
           if (newSearch) {
             setFeedPost(() => [...resp.data]);
-            // setThumbnail(() => [...thumbNailList]);
-            // setUserProfile(() => [...userProfileList]);
-            // setMember(() => [...memberList]);
-            // setHashTagList(() => [...hashTagLists]);
           } else {
             setFeedPost((prev) => [...prev, ...resp.data]);
-            // setThumbnail((prev) => [...prev, ...thumbNailList]);
-            // setUserProfile((prev) => [...prev, ...userProfileList]);
-            // setMember((prev) => [...prev, ...memberList]);
-            // setHashTagList((prev) => [...prev, ...hashTagLists]);
           }
-  
-          // setCpage((prev) => {
-          //   return (prev + 1);
-          // });
+
           console.log(resp.data);
           cpage.current = cpage.current + 1;
         })
-        .catch((error) =>{
+        .catch((error) => {
           console.log(error);
           pageLoading = false;
         });
     }
   };
-
-  // window.innerHeight 실제 보이는 창의 높이
-  // window.scrollY 페이지 상단에서부터 스크롤된 값
-  // document.body.offsetHeight 페이지 전체 높이
 
   return (
     <>
@@ -141,16 +112,7 @@ function SearchedFeedList() {
             <Masonry className={'my-masonry-grid'} options={masonryOptions}>
               {feedPost.map((e, i) => (
                 <div className="grid-item" key={i}>
-                  <FeedPostDetail
-                    index={i}
-                    // columnHeights={columnHeights}
-                    // setColumnHeights={setColumnHeights}
-                    feedPost={e}
-                    // thumbNail={thumbNail[i]}
-                    // member={member[i]}
-                    // userProfile={userProfile[i]}
-                    // hashTagList={hashTagList[i]}
-                  ></FeedPostDetail>
+                  <FeedPostDetail index={i} feedPost={e}></FeedPostDetail>
                 </div>
               ))}
             </Masonry>
@@ -164,25 +126,3 @@ function SearchedFeedList() {
 }
 
 export default SearchedFeedList;
-
-// window.innerHeight 실제 보이는 창의 높이
-// window.scrollY 페이지 상단에서부터 스크롤된 값
-// document.body.offsetHeight 페이지 전체 높이
-
-// 구현내용
-// (표시되는 영역 + 스크롤 값)이 (콘텐츠 전체 높이) 보다 클 때, 새로운 요소 추가.
-//    <section>
-//       <div class="box"></div>
-//       <div class="box">2번째</div>
-//    </section>
-// <script>
-//   var count = 2;
-
-// window.onscroll = function () {
-//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-//     var toAdd = document.createElement('div');
-//     toAdd.classList.add('box');
-//     toAdd.textContent = `${++count}번째`;
-//     document.querySelector('section').appendChild(toAdd);
-//   }
-// };
