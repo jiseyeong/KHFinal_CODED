8e7a1f4eaa269e8c36b2ab66fdb301eb8c36cccd
Sungha Kim <sungha0067@gmail.com>
2023-06-27 14:32:21 +0900

    Loading Bar Updated

--- FrontEnd/coded/src/App.js
+++ FrontEnd/coded/src/App.js
@@ -41,13 +41,12 @@ import MyPickPage from './pages/myPickPage/MyPickPage';
 import Follow from './test/Follow';
 import { styled } from 'styled-components';
 
-
 const ButtonContainer = styled.div`
   position: fixed;
   right: 5%;
   bottom: 5%;
   z-index: 1;
-`
+`;
 
 const TopButton = styled.button`
   font-weight: bold;
@@ -55,11 +54,11 @@ const TopButton = styled.button`
   padding: 15px 10px;
   /* background-color: #000;
   color:#fff */
-  border : 1px solid black;
+  border: 1px solid black;
   border-radius: 50%;
   outline: none;
   cursor: pointer;
-`
+`;
 
 function App() {
   const dispatch = useDispatch();
@@ -73,7 +72,7 @@ function App() {
     (refreshToken) => dispatch(setRefresh(refreshToken)),
     [dispatch],
   );
-  const accessToken = useSelector((state)=>state.member.access);
+  const accessToken = useSelector((state) => state.member.access);
   const [showTop, setShowTop] = useState(false);
 
   useEffect(() => {
@@ -95,28 +94,28 @@ function App() {
         onSetRefresh(refreshToken);
       })
       .catch((error) => {
-        if(accessToken){
+        if (accessToken) {
           onLogout();
         }
         console.log(error);
       });
 
-      window.addEventListener("scroll", handleShowTop);
-      return ()=>{
-        window.removeEventListener("scroll", handleShowTop);
-      }
+    window.addEventListener('scroll', handleShowTop);
+    return () => {
+      window.removeEventListener('scroll', handleShowTop);
+    };
   }, []);
 
-  function toTop(){
+  function toTop() {
     window.scrollTo({
-      top:0,
-      behavior:'smooth' // or auto
-    })
+      top: 0,
+      behavior: 'smooth', // or auto
+    });
   }
-  function handleShowTop(){
-    if(window.scrollY > 500){
+  function handleShowTop() {
+    if (window.scrollY > 500) {
       setShowTop(true);
-    }else{
+    } else {
       setShowTop(false);
     }
   }
@@ -139,10 +138,9 @@ function App() {
         <Route path="/pwSearch" element={<PwSearch />} />
         <Route path="/weekly" element={<WeeklyPage />} />
 
-        <Route path="/report" element={<Report/>}/>
+        <Route path="/report" element={<Report />} />
 
         <Route path="/deleteAccount" element={<DeleteAccount />} />
-
 
         <Route path="/testComponent" element={<TestComponent />} />
         <Route path="/imageUpload" element={<ImageUpload />} />
@@ -181,7 +179,19 @@ function App() {
       </Routes>
       {showTop && (
         <ButtonContainer>
-          <TopButton onClick={toTop}>Top</TopButton>
+          <TopButton onClick={toTop}>
+            <svg
+              height="20"
+              viewBox="0 0 20 20"
+              width="20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4.14708 12.3544C3.95147 12.1594 3.9509 11.8429 4.14582 11.6472L9.6108 6.16276C9.82574 5.94705 10.1751 5.94705 10.39 6.16276L15.855 11.6472C16.0499 11.8429 16.0493 12.1594 15.8537 12.3544C15.6581 12.5493 15.3415 12.5487 15.1466 12.3531L10.0004 7.18851L4.85418 12.3531C4.65927 12.5487 4.34269 12.5493 4.14708 12.3544Z"
+                fill="#222"
+              />
+            </svg>
+          </TopButton>
         </ButtonContainer>
       )}
       <Footer />
