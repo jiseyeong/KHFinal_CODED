e1fab7f924459932eab057c4a39f5b6d656a3a80
LJHHKF <43880473+LJHHKF@users.noreply.github.com>
2023-06-29 15:02:23 +0900

    Admin user list Added & ended

--- BackEnd/Coded/src/main/java/kh/coded/services/MemberService.java
+++ BackEnd/Coded/src/main/java/kh/coded/services/MemberService.java
@@ -1,6 +1,9 @@
 package kh.coded.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import kh.coded.dto.MyPickPageDTO;
@@ -176,6 +179,10 @@ public class MemberService implements UserDetailsService {
 		} else {
 			return 0;
 		}
+	}
+	
+	public int deleteMember_Admin(int userNo) {
+		return memberDAO.deleteMemberAdmin(userNo);
 	}
 
 	public int updateMember(MemberDTO dto) {
@@ -516,5 +523,58 @@ public class MemberService implements UserDetailsService {
 
 	public MyPickPageDTO selectMyPickPageData(int userNo) {
 		return memberDAO.selectMyPickPageData(userNo);
+	}
+	
+	public Map<String, Object> selectPageNavi(int cpage){
+		//1. 전체 글의 개수
+		int recordTotalCount = memberDAO.getRecordCount();
+		//2. 페이지당 보여줄 글의 개수
+		int recordCountPerPage = StaticValue.FEEDCOUNTPERSCROLL;
+		//3. 페이지당 보여줄 네비게이터의 수
+		int naviCountPerPage = StaticValue.NAVICOUNTPERPAGE;
+
+		//4. 1번과 2번 항목에 의해 총 페이지의 개수가 정해짐.
+		//전체 글의 개수를 페이지당 
+		int pageTotalCount = recordTotalCount%recordCountPerPage > 0 ?
+				recordTotalCount/recordCountPerPage + 1
+				:recordTotalCount/recordCountPerPage;
+
+		if(cpage < 1) {
+			cpage = 1;
+		}else if(cpage > pageTotalCount) {
+			cpage = pageTotalCount;
+		}
+
+		int startNavi = (cpage-1)/naviCountPerPage*naviCountPerPage+1;
+		int endNavi = startNavi + (naviCountPerPage-1);
+
+		if(endNavi > pageTotalCount) {
+			endNavi = pageTotalCount;
+		}
+
+		boolean needPrev = true;
+		boolean needNext = true;
+		ArrayList<Integer> list = new ArrayList<>();
+
+		if(startNavi == 1) {needPrev = false;}
+		if(endNavi == pageTotalCount) {needNext = false;}
+		for(int i = startNavi; i <= endNavi; i++) {
+			list.add(i);
+		}
+
+		Map<String, Object> data = new HashMap<>();
+		data.put("naviList", list);
+		data.put("needPrev", needPrev);
+		data.put("needNext", needNext);
+
+		return data;
+	}
+	
+	public List<MemberDTO> pagingMember(int cpage) {
+		int feedCountPerPage = StaticValue.FEEDCOUNTPERSCROLL;
+		int endNum = cpage * feedCountPerPage;
+		int startNum = endNum - (feedCountPerPage - 1);
+		
+		return memberDAO.selectPaging(startNum, endNum);
 	}
 }
