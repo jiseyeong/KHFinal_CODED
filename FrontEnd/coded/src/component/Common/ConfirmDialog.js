9722f7d0d1fee010083275fe8143cc5f4e67cb28
Sungha Kim <sungha0067@gmail.com>
2023-06-30 14:36:34 +0900

    CommentListHeart Updated

--- FrontEnd/coded/src/component/Common/ConfirmDialog.js
+++ FrontEnd/coded/src/component/Common/ConfirmDialog.js
@@ -1,48 +1,53 @@
 import {
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-    Button,
-    Box,
-    IconButton,
-    Typography,
-  } from '@material-ui/core';
-  import { Close } from '@material-ui/icons';
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Box,
+  IconButton,
+  Typography,
+} from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import { useNavigate } from 'react-router-dom';
-  
-  
-  const ConfirmDialog = (props) => {
-    const setAlertCheck  = props.setAlertCheck;
-    const navi = useNavigate();
-    const test = ()=>{
-      setAlertCheck(false)
-    }
-    const test2 = () => {
-      navi("/login");
-    }
 
-    return (
-      <Dialog open={true} maxWidth="sm" fullWidth>
-        <DialogTitle>로그인 확인</DialogTitle>
-        <Box position="absolute" top={0} right={0}>
-          <IconButton onClick={test}>
-            <Close />
-          </IconButton>
-        </Box>
-        <DialogContent>
-          <Typography>로그인 후에 이용이 가능합니다</Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" variant="contained" onClick={()=>{setAlertCheck(false)}}>
-            취소
-          </Button>
-          <Button color="secondary" variant="contained" onClick={test2}>
-            로그인
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
+const ConfirmDialog = (props) => {
+  const setAlertCheck = props.setAlertCheck;
+  const navi = useNavigate();
+  const test = () => {
+    setAlertCheck(false);
   };
-  
-  export default ConfirmDialog;
+  const test2 = () => {
+    navi('/login');
+  };
+
+  return (
+    <Dialog open={true} maxWidth="sm" fullWidth>
+      <DialogTitle>MEMBER-ONLY SERVICE!</DialogTitle>
+      <Box position="absolute" top={0} right={0}>
+        <IconButton onClick={test}>
+          <Close />
+        </IconButton>
+      </Box>
+      <DialogContent>
+        <Typography>로그인 후 이용해주세요</Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => {
+            setAlertCheck(false);
+          }}
+        >
+          cancel
+        </Button>
+        <Button color="secondary" variant="contained" onClick={test2}>
+          login
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmDialog;
