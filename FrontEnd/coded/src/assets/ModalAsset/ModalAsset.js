5212002900a77aa91fb2a83f0363762e10581c35
gunheelee22 <sapple90@nate.com>
2023-07-03 09:17:33 +0900

    report up

--- FrontEnd/coded/src/assets/ModalAsset/ModalAsset.js
+++ FrontEnd/coded/src/assets/ModalAsset/ModalAsset.js
@@ -30,6 +30,7 @@ const Like = (handleClickLike) => {
       width="1em"
       xmlns="http://www.w3.org/2000/svg"
     >
+      
       <path
         fillRule="evenodd"
         d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
